42a7df39fb2e5338db4739ca11960db054fd8e55
fullcorder <fullcorder@gmail.com>
2017-12-27 19:26:54 +0900

    Create Factory class

---
+++ Assets/LocalizedText/Scripts/SingletonTextSet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LocalText;
+using UnityEngine;
+
+namespace LocalizedText
+{
+    public static class SingletonTextSet
+    {
+        private const string ResourcesPath = "TextSet";
+
+        private static TextSet instance;
+
+        private static readonly object lockObject = new Object();
+
+        public static TextSet Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    lock(lockObject)
+                    {
+                        if(instance == null)
+                        {
+                            instance = Resources.Load<TextSet>(ResourcesPath);
+                            LanguageSwitcher.Create();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
