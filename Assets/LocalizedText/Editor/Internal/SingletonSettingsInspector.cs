e2871251fde925c476e3f53e552be6891aa82855
fullcorder <fullcorder@gmail.com>
2017-12-27 19:23:32 +0900

    Create Singleton Settings

---
+++ Assets/LocalizedText/Editor/Internal/SingletonSettingsInspector.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using LocalizedText.Editor.Internal;
+using LocalizedText.Importer;
+using LocalText.Internal;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace LocalizedText.Internal
+{
+    [CustomEditor(typeof(SingletonSettings))]
+    public class SingletonSettingsInspector : SettingsInspector
+    {
+        public override void OnInspectorGUI()
+        {
+            var settings = serializedObject.targetObject as Settings;
+
+            serializedObject.Update();
+
+            GUILayout.Label("Asset Name", BoldLabelStyle);
+            EditorGUILayout.LabelField(settings.TextSetAssetName);
+
+            GUILayout.Label("Instance Type", BoldLabelStyle);
+            EditorGUILayout.LabelField("Singleton (Use Resources folder)");
+
+            EditorGUILayout.Space();
+
+            EditorGUI.BeginChangeCheck();
+
+            GUILayout.Label("Language", BoldLabelStyle);
+            reorderableList.DoLayoutList();
+            serializedObject.ApplyModifiedProperties();
+
+            GUILayout.Label("Genrete TextSet Path", BoldLabelStyle);
+            EditorGUILayout.LabelField(settings.TextSetAssetPath());
+
+            GUILayout.Label("Genrete KeyFile Path", BoldLabelStyle);
+            EditorGUILayout.LabelField(settings.KeyAssetPath());
+
+            EditorGUILayout.Space();
+
+            GUILayout.Label("Data Source", BoldLabelStyle);
+            EditorGUILayout.LabelField(settings.SelectedDataSource.ToString());
+
+            EditorGUILayout.Space();
+
+            if(settings.SelectedDataSource == Settings.DataSource.GoogleSpreadSheetAsWeb)
+            {
+                GUILayout.Label("Google spread sheet URL");
+                settings.GoogleSpreadSheetUrl = EditorGUILayout.TextField(settings.GoogleSpreadSheetUrl);
+
+                EditorGUILayout.Space();
+
+                EditorGUILayout.BeginHorizontal();
+                if(GUILayout.Button("CreateAll or Update"))
+                {
+                    LocalizedTextLogger.Log(LocalizedTextLogger.LogLevel.Debug, "CreateAll Button Click");
+                    GoogleSpreadSheetImporter.SyncGoogleSpreadSheetApi(settings);
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+
+            if(EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(settings);
+                AssetDatabase.SaveAssets();
+            }
+        }
+    }
+}
