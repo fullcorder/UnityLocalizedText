7afdf328a3ff56f466ea44f9a67f4f213584aec2
fullcorder <fullcorder@gmail.com>
2017-12-27 19:26:21 +0900

    Add auto language switch feature

---
+++ Assets/LocalizedText/Scripts/LanguageSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace LocalizedText
+{
+    public class LanguageSwitcher : MonoBehaviour
+    {
+        private static LanguageSwitcher languageSwitcher;
+
+        public static void Create()
+        {
+            if(languageSwitcher != null)
+            {
+                return;
+            }
+
+            var o = new GameObject()
+            {
+                name = "~LanguageSwitcher",
+                hideFlags = HideFlags.HideInHierarchy
+            };
+            DontDestroyOnLoad(o);
+            languageSwitcher = o.AddComponent<LanguageSwitcher>();
+        }
+
+        private void OnApplicationPause(bool pauseStatus)
+        {
+            if(pauseStatus)
+            {
+                return;
+            }
+
+            var currentLangueage = Application.systemLanguage;
+            var textSet = SingletonTextSet.Instance;
+
+            if(textSet.CurrentSystemLanguage == currentLangueage)
+            {
+                return;
+            }
+
+            textSet.CurrentSystemLanguage = currentLangueage;
+        }
+    }
+}
