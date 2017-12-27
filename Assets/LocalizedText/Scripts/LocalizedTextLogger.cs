afb2453d0b08bac2907e0346d827354ad2d3ede7
fullcorder <fullcorder@gmail.com>
2017-12-27 19:28:31 +0900

    Add Logger class

---
+++ Assets/LocalizedText/Scripts/LocalizedTextLogger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace LocalText.Internal
+{
+    public static class LocalizedTextLogger
+    {
+        public enum LogLevel
+        {
+            None = 0,
+            Info = 1,
+            Debug = 2,
+            Verbose = 3,
+        }
+
+        public static LogLevel CurrentLogLevel { get; set; }
+
+        public static ILogger logger { private get; set; }
+
+        static LocalizedTextLogger()
+        {
+            logger = UnityEngine.Debug.logger;
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Log(LogLevel logLevel, string message)
+        {
+            if(logLevel < CurrentLogLevel)
+            {
+                return;
+            }
+
+            logger.Log(LogType.Log, message);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void LogFormat(LogLevel logLevel, string message, params object[] args)
+        {
+            if(logLevel < CurrentLogLevel)
+            {
+                return;
+            }
+
+            logger.LogFormat(LogType.Log, message, args);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Assert(Action<bool> condition, string message = "")
+        {
+            logger.Log(LogType.Assert, message);
+        }
+    }
+}
