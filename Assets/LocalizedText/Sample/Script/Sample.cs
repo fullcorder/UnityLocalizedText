711d22581b100f006bd9e914273e33b86d373b6f
fullcorder <fullcorder@gmail.com>
2017-12-29 15:23:48 +0900

    Create Sample Scene

---
+++ Assets/LocalizedText/Sample/Script/Sample.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace LocalizedText
+{
+    public class Sample : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _text;
+
+        private void Start()
+        {
+            var textSet = SingletonTextSet.Instance;
+
+            _text.text += textSet.Text(TextSetKey.title);
+            _text.text += "\n";
+            _text.text += textSet.Format(TextSetKey.hello_text, Application.systemLanguage.ToString());
+            _text.text += "\n";
+            _text.text += textSet[TextSetKey.dummy_text]; //Text method also define as indexer method.
+        }
+    }
+}
