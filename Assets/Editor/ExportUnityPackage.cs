213812cf82b2d1ec89921d59017918591bb637aa
fullcorder <s-kobayashi@mgaicant.co.jp>
2018-08-04 20:58:30 +0900

    UnityPackage Export Script

---
+++ Assets/Editor/ExportUnityPackage.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+public static class ExportUnityPackage
+{
+    private static readonly string Version = "0.3.0";
+
+    [MenuItem("LocalizeText/Export")]
+    public static void Export()
+    {
+        AssetDatabase.ExportPackage("Assets/LocalizedText",
+            string.Format("UnityLocalizeText{0}.unitypackage", Version), ExportPackageOptions.Recurse);
+    }
+}
